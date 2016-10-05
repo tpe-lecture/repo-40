65fb84fad87c6b4a5b5c5b742593a5c716ed3232
Thomas Smits <thomsmits@gmail.com>
2016-10-05 13:02:00 +0200

    I Robot

---
+++ 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/test/TestWesen.java
@@ -0,0 +1,43 @@
+package tpe.oo.constructor.test;
+
+import org.junit.Test;
+
+/*
+import tpe.constructor.Wesen;
+import tpe.constructor.Nachtelf;
+import tpe.constructor.Untoter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+*/
+/**
+ * Test für die Aufgabe.
+ */
+public class TestWesen {
+
+    /**
+     * Testet den Nachtelf.
+     */
+    @Test
+    public void testNachtelf() {
+        /*
+        assertTrue("Nachtelf erbt nicht von Figur", new Nachtelf("") instanceof Wesen);
+        assertEquals(Nachtelf.STANDARD_NATURWIDERSTAND, new Nachtelf("").getNaturwiderstand());
+        assertEquals(120, new Nachtelf("", 120).getNaturwiderstand());
+        assertEquals("Hugo", new Nachtelf("Hugo").getName());
+        */
+    }
+    
+    /**
+     * Testet den Untoten.
+     */
+    @Test
+    public void testUntoten() {
+        /*
+        assertTrue("Untoter erbt nicht von Figur", new Untoter("") instanceof Wesen);
+        assertEquals(Untoter.STANDARD_UNTERWASSERATMUNG, new Untoter("").getUnterwasseratmung());
+        assertEquals(120, new Untoter("", 120).getUnterwasseratmung());
+        assertEquals("Hugo", new Untoter("Hugo").getName());
+        */
+    }
+}
