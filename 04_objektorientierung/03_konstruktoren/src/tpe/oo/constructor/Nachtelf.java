ba65faf78dd6fa476eb2b1158a08dac349af5bd2
d.kassube <d.kassube@bw-lp>
2016-10-24 11:53:53 +0200

    Aufgabe Konstruktoren gelöst

--- 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/Nachtelf.java
+++ 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/Nachtelf.java
@@ -3,7 +3,7 @@ package tpe.oo.constructor;
 /**
  * Nachtelf aus der Allianz.
  */
-public class Nachtelf /* extends Figur */ {
+public class Nachtelf extends Wesen {
 
     /** Standardmäßige Stärke der Fähigkeit Naturwiderstand. */
     public static final int STANDARD_NATURWIDERSTAND = 5;
@@ -11,8 +11,14 @@ public class Nachtelf /* extends Figur */ {
     /** Fähigkeit zum Widerstand gegen Naturmagie */
     private int naturwiderstand;
     
-    // Konstruktoren fehlen. Bitte hier einfügen.
-    
+    public Nachtelf(String name){
+        super(name);
+        this.naturwiderstand = STANDARD_NATURWIDERSTAND;
+    }
+    public Nachtelf(String name, int naturwiderstand) {
+        super(name);
+        this.naturwiderstand = naturwiderstand;
+        }
     /**
      * Stärke des Naturwiderstandes.
      * 
--- 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/test/TestWesen.java
+++ 04_objektorientierung/03_konstruktoren/src/tpe/oo/constructor/test/TestWesen.java
@@ -2,14 +2,14 @@ package tpe.oo.constructor.test;
 
 import org.junit.Test;
 
-/*
-import tpe.constructor.Wesen;
-import tpe.constructor.Nachtelf;
-import tpe.constructor.Untoter;
+
+import tpe.oo.constructor.Wesen;
+import tpe.oo.constructor.Nachtelf;
+import tpe.oo.constructor.Untoter;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-*/
+
 /**
  * Test für die Aufgabe.
  */
@@ -20,12 +20,12 @@ public class TestWesen {
      */
     @Test
     public void testNachtelf() {
-        /*
+        
         assertTrue("Nachtelf erbt nicht von Figur", new Nachtelf("") instanceof Wesen);
         assertEquals(Nachtelf.STANDARD_NATURWIDERSTAND, new Nachtelf("").getNaturwiderstand());
         assertEquals(120, new Nachtelf("", 120).getNaturwiderstand());
         assertEquals("Hugo", new Nachtelf("Hugo").getName());
-        */
+        
     }
     
     /**
@@ -33,11 +33,11 @@ public class TestWesen {
      */
     @Test
     public void testUntoten() {
-        /*
+        
         assertTrue("Untoter erbt nicht von Figur", new Untoter("") instanceof Wesen);
         assertEquals(Untoter.STANDARD_UNTERWASSERATMUNG, new Untoter("").getUnterwasseratmung());
         assertEquals(120, new Untoter("", 120).getUnterwasseratmung());
         assertEquals("Hugo", new Untoter("Hugo").getName());
-        */
+        
     }
 }
